110a2e4e7c75e3ba20d1a0986bb7f21e8c94b272
chandou <116485511+chandou-code@users.noreply.github.com>
2024-10-30 18:20:50 +0800

    Update TicTok.py

--- TicTok.py
+++ TicTok.py
@@ -12,23 +12,37 @@ def press_space():
 
 
 def start(file_path):
-    # 启动程序
-    print('start')
     process = subprocess.Popen(file_path)
-    time.sleep(0.3)  # 等待程序启动
+    time.sleep(0.1)  # 等待程序启动
 
-    press_space()
+    # 获取窗口标题
+    window_title = "抖音"
+    windows = gw.getWindowsWithTitle(window_title)
 
+    if windows:
+        window = windows[0]  # 获取第一个窗口
+        window.activate()  # 激活窗口
+        time.sleep(0.5)  # 确保窗口获得焦点
+
+        press_space()  # 发送空格键
+    else:
+        print("未找到窗口")
+
+def start_(file_path,window_title):
+    start(file_path)
 
 def mini(window_title):
     # 获取当前窗口列表
     time.sleep(0.1)
-    print('end')
+    # print('end')
     windows = gw.getWindowsWithTitle(window_title)
     print("当前窗口:", windows)
     # 如果找到了窗口，则最小化
     if windows:
         window = windows[0]  # 假设找到的第一个窗口是我们要操作的窗口
+        window.activate()  # 激活窗口
+        time.sleep(0.5)
+        press_space()  # 发送空格键
         window.minimize()  # 最小化窗口
         print("程序窗口已最小化")
     else:
@@ -36,8 +50,8 @@ def mini(window_title):
 
 
 def mini_(window_title, file_path):
-    mini(window_title)
-    start(file_path)
+    # mini(window_title)
+    # start(file_path)
     mini(window_title)
 
 
@@ -46,7 +60,7 @@ def toggle_window():
     file_path = r"E:\douyin\4.8.1\douyin.exe"
     window_title = "抖音"
     if current_state:
-        start(file_path)
+        start_(file_path,window_title)
     else:
 
         mini_(window_title, file_path)
