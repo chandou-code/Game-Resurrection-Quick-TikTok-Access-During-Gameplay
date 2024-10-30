35b05fde5ed0d5aa9dfd3f88571c301d0cc1b9d7
chandou <116485511+chandou-code@users.noreply.github.com>
2024-10-30 16:40:16 +0800

    Update TicTok.py

--- TicTok.py
+++ TicTok.py
@@ -5,6 +5,7 @@ from pynput import mouse, keyboard
 current_state = True
 
 def press_space():
+    print(111)
     controller = keyboard.Controller()
     controller.press(keyboard.Key.space)
     controller.release(keyboard.Key.space)
@@ -12,6 +13,7 @@ def press_space():
 
 def start(file_path):
     # 启动程序
+    print('start')
     process = subprocess.Popen(file_path)
     time.sleep(0.3)  # 等待程序启动
 
@@ -20,7 +22,8 @@ def start(file_path):
 
 def mini(window_title):
     # 获取当前窗口列表
-
+    time.sleep(0.1)
+    print('end')
     windows = gw.getWindowsWithTitle(window_title)
     print("当前窗口:", windows)
     # 如果找到了窗口，则最小化
@@ -34,7 +37,6 @@ def mini(window_title):
 
 def mini_(window_title, file_path):
     mini(window_title)
-
     start(file_path)
     mini(window_title)
 
@@ -46,6 +48,7 @@ def toggle_window():
     if current_state:
         start(file_path)
     else:
+
         mini_(window_title, file_path)
 
     current_state = not current_state
@@ -57,7 +60,13 @@ def on_click(x, y, button, pressed):
 
 if __name__ == '__main__':
 
-    # 开始监听鼠标事件
+
     with mouse.Listener(on_click=on_click) as listener:
         print("按下鼠标侧键 4 以切换窗口状态（恢复/最小化）...")
         listener.join()  # 持续监听
+    # file_path = r"E:\douyin\4.8.1\douyin.exe"
+    # window_title = "抖音"
+    #
+    # # 启动程序
+    # process = subprocess.Popen(file_path)
+
