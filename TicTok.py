7b8ebfebb108caf99d2a5e4b26ad983f561a02fe
chandou <116485511+chandou-code@users.noreply.github.com>
2024-10-30 16:09:13 +0800

    Update TicTok.py

--- TicTok.py
+++ TicTok.py
@@ -1,73 +1,68 @@
-import pygetwindow as gw
-import pynput.mouse as mouse
-import time
+import subprocess
 import time
 import pygetwindow as gw
 from pynput import mouse, keyboard
-current_state = False  # False 表示窗口最小化，True 表示窗口恢复
+current_state = True
+
 def press_space():
     controller = keyboard.Controller()
     controller.press(keyboard.Key.space)
     controller.release(keyboard.Key.space)
 
-def restore_window(window_title, target_hWnd):
+
+def start(file_path):
+    # 启动程序
+    process = subprocess.Popen(file_path)
+    time.sleep(0.3)  # 等待程序启动
+
+    press_space()
+
+
+def mini(window_title):
+    # 获取当前窗口列表
+    
     windows = gw.getWindowsWithTitle(window_title)
-    for window in windows:
-        if window._hWnd == target_hWnd:
-            if window.isMinimized:
-                window.restore()
-                time.sleep(0.1)  # 确保窗口恢复后再进行激活
-            window.activate()
-            press_space()
-            print(f"窗口 '{window_title}' (hWnd={target_hWnd}) 已被激活。")
-            return
-    print(f"没有找到标题为 '{window_title}' 且句柄为 '{target_hWnd}' 的窗口。")
+    print("当前窗口:", windows)
+    # 如果找到了窗口，则最小化
+    if windows:
+        window = windows[0]  # 假设找到的第一个窗口是我们要操作的窗口
+        window.minimize()  # 最小化窗口
+        print("程序窗口已最小化")
+    else:
+        print("未找到窗口")
 
-def minimize_window(window_title, target_hWnd):
-    windows = gw.getWindowsWithTitle(window_title)
-    for window in windows:
-        if window._hWnd == target_hWnd:
-            window.minimize()
-            print(f"窗口 '{window_title}' (hWnd={target_hWnd}) 已被最小化。")
 
-            return
+def mini_(window_title, file_path):
+    mini(window_title)
 
-    print(f"没有找到标题为 '{window_title}' 且句柄为 '{target_hWnd}' 的窗口。")
-
-def get_second_max_hWnd(window_title):
-    windows = gw.getWindowsWithTitle(window_title)
-    if len(windows) < 2:  # 如果窗口数量少于2，返回None
-        return None
-    hWnds = sorted(window._hWnd for window in windows)  # 按照hWnd排序
-    return hWnds[-2]  # 返回第二大的hWnd
+    start(file_path)
+    mini(window_title)
 
 
 def toggle_window():
     global current_state
+    file_path = r"E:\douyin\4.8.1\douyin.exe"
     window_title = "抖音"
-    target_hWnd = get_second_max_hWnd(window_title)  # 获取最大 hWnd
-    # target_hWnd = 265778  # 获取最大 hWnd
-
-    if target_hWnd is None:
-        print(f"没有找到标题为 '{window_title}' 的窗口。")
-        return
-
     if current_state:
-        minimize_window(window_title, target_hWnd)
-        restore_window(window_title, target_hWnd)
-        minimize_window(window_title, target_hWnd)
+        start(file_path)
     else:
-        restore_window(window_title, target_hWnd)
+        mini_(window_title, file_path)
 
+    current_state = not current_state
 
-    current_state = not current_state  # 切换状态
-
-# 监听鼠标侧键 4
 def on_click(x, y, button, pressed):
     if button == mouse.Button.x2 and pressed:  # 检查是否按下侧键 4
         toggle_window()
 
-# 开始监听鼠标事件
-with mouse.Listener(on_click=on_click) as listener:
-    print("按下鼠标侧键 4 以切换窗口状态（恢复/最小化）...")
-    listener.join()  # 持续监听
+
+if __name__ == '__main__':
+    file_path = r"E:\douyin\4.8.1\douyin.exe"
+    window_title = "抖音"
+    # start(file_path)
+    # mini_(window_title, file_path)
+
+    #
+    # 开始监听鼠标事件
+    with mouse.Listener(on_click=on_click) as listener:
+        print("按下鼠标侧键 4 以切换窗口状态（恢复/最小化）...")
+        listener.join()  # 持续监听
